48a9e13f81915a2605712638cd5d35625153dd5b
Thien Duong <thien.duong@newoceaninfosys.com>
2018-06-14 13:37:32 +0700

    apply cors

--- API/Startup.cs
+++ API/Startup.cs
@@ -12,6 +12,7 @@ namespace API
     {
         public void Configuration(IAppBuilder app)
         {
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             ConfigureAuth(app);
         }
     }
