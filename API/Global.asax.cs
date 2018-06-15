3e61157997b8b242f0f1b1046909b7be65f52423
Thien Duong <thien.duong@newoceaninfosys.com>
2018-06-15 08:43:29 +0700

    add file

---
+++ API/Global.asax.cs
@@ -0,0 +1,48 @@
+﻿using API.Framework;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace API
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            AutoMapperConfiguration.Configure();
+        }
+    }
+
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                var drTypes = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes())
+                    .Where(type => !String.IsNullOrEmpty(type.Namespace))
+                    .Where(type => type.GetInterfaces().Contains(typeof(IMapperRegistrar)));
+
+                var drInstances = new List<IMapperRegistrar>();
+                foreach (var drType in drTypes)
+                    drInstances.Add((IMapperRegistrar)Activator.CreateInstance(drType));
+                //sort
+                drInstances = drInstances.AsQueryable().OrderBy(t => t.Order).ToList();
+                foreach (var mapper in drInstances)
+                    mapper.Register(cfg);
+            });
+        }
+    }
+}
